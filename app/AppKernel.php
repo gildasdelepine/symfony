1d359071741f195a10c861f5a6574c0333faab3a
Gildas DELEPINE <gildas.delepine@smile.fr>
2018-10-10 15:23:02 +0200

    Utiliser Assetic pour gérer les codes CSS et JS de votre site
    - Pour exporter les fichiers CSS et JS pour le mode prod (depuis le serveur)
    1. Vider les caches (prod et dev)
    php bin/console cache:clear --env=prod
    php bin/console cache:clear
    
    2. lancer la commande d'export
    php bin/console assetic:dump --env=prod

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new SML\UserBundle\SMLUserBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
         );
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
